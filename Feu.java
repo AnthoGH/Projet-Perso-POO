82162a9e0700d9167b275e243066d16ab8113c3f
AnthoGH <anthogeorg69@gmail.com>
2017-11-30 11:37:02 +0100

    Create Feu.java

---
+++ Feu.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package progobject;
+
+/**
+ *
+ * @author Antho
+ */
+public class Feu {
+    private String couleur;
+
+    public String getCouleur() {
+        return couleur;
+    }
+
+    public void setCouleur(String couleur) {
+        this.couleur = couleur;
+    }
+
+    
+    
+}
